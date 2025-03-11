ac7f92f978108458dece0293c52d875a90667e0e
krishpatel1310 <patelkrish7568@gmail.com>
2025-03-11 23:49:39 +0530

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use-strict"; //treat all js code as newer version
+
+//alert(3 + 3) //we are using nodejs, not browser
+
+//code readabilty should be high
+console.log("Krish Patel");
+
+let name = "Krish";
+let age = 22;
+let isLoggedIn = true;
+let state;
+
+// number => 2 range of 52
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unqiue
+
+//object
+
+console.log(typeof undefined);
