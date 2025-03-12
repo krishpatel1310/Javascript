9d9f84f06b59864edd207016aed1ea48accb20a5
krishpatel1310 <patelkrish7568@gmail.com>
2025-03-12 22:45:15 +0530

    conversion pf datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "123";
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+// "123" => Number
+// "123krish" => Not A Number(NaN)
+// "true => 1, false => 0" === Boolean
+
+let isLoggedIn = "";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 0 => False, 1 => True
+// "" => False
+// "abc" => True
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
