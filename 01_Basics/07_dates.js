78f4b998aa3278cb30f3d1ef489c4982331aad9e
krishpatel1310 <patelkrish7568@gmail.com>
2025-03-23 20:17:08 +0530

    dates and time

---
+++ 01_Basics/07_dates.js
@@ -0,0 +1,34 @@
+// Dates
+
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toDateString());
+
+console.log(typeof myDate);
+
+// let createdDate = new Date(2020, 2, 22);
+// let createdDate = new Date(2020, 2, 22, 5, 5);
+let createdDate = new Date("10-13-2023");
+//console.log(createdDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(createdDate.getTime());
+// console.log(createdDate.getSeconds());
+
+// console.log(Math.round(Date.now() / 1000));
+
+let newDate = new Date();
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay() + 1);
+
+// `${newDate.getDate} and the time is`
+
+// newDate.toLocaleString("default", {
+//   weekday: "long",
+// });
